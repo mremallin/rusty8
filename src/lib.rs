a69d5be8aa2f9b353aa92b5971830c4cdb118d68
Mike Mallin <mike@mremallin.ca>
2021-09-12 12:17:26 -0400

    Refactor 0x8xy1 test for readability

--- src/lib.rs
+++ src/lib.rs
@@ -518,21 +518,21 @@ mod chip8_tests {
     fn opc_8xy1() {
         let mut c8i = Chip8Instance::default();
 
-        for i in (0x8001..0x9000).step_by(0x100) {
-            for j in (0..0xF1).step_by(0x10) {
-                let op = (i & 0xFF0F) | j;
+        for i in 0..Chip8Instance::NUM_V_REGISTERS {
+            for j in 0..Chip8Instance::NUM_V_REGISTERS {
+                let op = build_xyn_opc(8, i as u8, j as u8, 1);
 
                 /* Set destination with a known value */
-                interpret_instruction(&mut c8i, 0x6005 | (op & 0x0f00));
+                interpret_instruction(&mut c8i, build_xnn_opc(6, i as u8, 5));
                 /* Set source to test value */
-                interpret_instruction(&mut c8i, 0x60a0 | ((j & 0xf0) << 4));
+                interpret_instruction(&mut c8i, build_xnn_opc(6, j as u8, 0xa0));
                 /* Set Vx = Vx | Vy */
                 interpret_instruction(&mut c8i, op);
 
-                if ((i & 0x0F00) >> 4) != (j & 0xF0) {
-                    assert_eq!(c8i.v_regs[((i & 0x0F00) >> 8) as usize], 0xa5);
+                if i != j {
+                    assert_eq!(c8i.v_regs[i], 0xa5);
                 } else {
-                    assert_eq!(c8i.v_regs[((i & 0x0F00) >> 8) as usize], 0xa0);
+                    assert_eq!(c8i.v_regs[j], 0xa0);
                 }
             }
         }
